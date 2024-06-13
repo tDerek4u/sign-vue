f6490b36d3cd9e3570070939304efc528c2cfa5c
tDerek4u <zwehtetmhan433@gmail.com>
2024-06-13 09:33:15 +0700

    spaw

---
+++ src/views/Login.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="w-full " style="height: 100vh;">
+        <div class="flex justify-between">
+            <div class="w-1/2 ">
+                <img src="https://api.asm.skype.com/v1/objects/0-sa-d5-dd8a0008b98799a1a094678bb19c7957/views/imgpsh_fullsize_anim" class="" style="height: 100vh;" alt="login">
+            </div>
+            <div class="w-1/2 ">
+                <div class="flex justify-center items-center h-full">
+                   
+                        <h1>Hi</h1>
+                   
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
